b3d13193c9171d89767866210a598c20a328cc03
rookie996-object <472099249@qq.com>
2022-03-22 23:00:29 +0800

    perf: adjustment base css

--- src/components/SideNav.vue
+++ src/components/SideNav.vue
@@ -2,7 +2,7 @@
   <div
     class="hidden lg:block fixed z-20 inset-0 top-[3.8125rem] left-[max(0px,calc(50%-45rem))] right-auto w-[19.5rem] pb-10 px-8 overflow-y-auto"
   >
-    <nav class="lg:text-sm lg:leading-6 relative">
+    <nav class="lg:text-base lg:leading-6 relative">
       <ul>
         <li class="mt-12 lg:mt-8">
           <h5
