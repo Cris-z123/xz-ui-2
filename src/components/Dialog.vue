4644a2f176f50a07b38162f4ed904a33c05ec4e5
rookie996-object <472099249@qq.com>
2022-04-24 21:53:08 +0800

    feat<Dialog>: Dialog components style

---
+++ src/components/Dialog.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="fixed w-full h-full bg-white z-40"></div>
+  <div class="fixed top-2/4 left-2/4 shadow z-50">
+    <header class="flex items-center justify-between px-4 py-3 border-b">
+      <h3>标题</h3>
+      <Button icon="close" type="text" />
+    </header>
+    <main class="px-4 py-3">内容</main>
+    <footer class="text-right px-4 py-3 border-t">
+      <Button size="small">确认</Button>
+      <Button type="default" size="small">取消</Button>
+    </footer>
+  </div>
+</template>
+
+<script setup lang="ts">
+import Button from './Button.vue';
+</script>
