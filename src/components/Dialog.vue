e6dca66c847358b370d8eed96efd0cd853cfafc5
rookie996-object <472099249@qq.com>
2024-07-31 21:28:49 +0800

    :sparkles: feat(dialog): 功能20%

--- src/components/Dialog.vue
+++ src/components/Dialog.vue
@@ -1,13 +1,14 @@
 <template>
   <div class="fixed w-full h-full bg-white z-40"></div>
-  <div class="fixed top-2/4 left-2/4 shadow z-50">
+  <div v-if="props.visible" class="fixed top-2/4 left-2/4 shadow z-50">
     <header class="flex items-center justify-between px-4 py-3 border-b">
-      <h3>标题</h3>
+      <h3>{{ props.title }}</h3>
       <Button icon="close" type="text" />
     </header>
     <main class="px-4 py-3">内容</main>
+    <slot></slot>
     <footer class="text-right px-4 py-3 border-t">
-      <Button size="small">确认</Button>
+      <Button size="small">{{ props.confirmText }}</Button>
       <Button type="default" size="small">取消</Button>
     </footer>
   </div>
@@ -15,4 +16,18 @@
 
 <script setup lang="ts">
 import Button from './Button.vue';
+
+const props = defineProps({
+  visible: {
+    type: Boolean,
+    required: true
+  },
+  title: {
+    type: String
+  },
+  confirmText: {
+    type: String,
+    default: '确认'
+  }
+});
 </script>
