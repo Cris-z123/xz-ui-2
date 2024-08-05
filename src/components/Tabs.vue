563bd59ca2d8d57a10ca76d2af8aca45987016b8
rookie996-object <472099249@qq.com>
2024-08-05 23:41:50 +0800

    :sparkles: feat(tabs): tabs基本内容

--- src/components/Tabs.vue
+++ src/components/Tabs.vue
@@ -1,5 +1,22 @@
 <template>
-  <div>tabs</div>
+  <div class="tabs">
+    <slot></slot>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const props = defineProps({
+  selected: {
+    type: String,
+    required: true
+  },
+  direction: {
+    type: String,
+    default: 'horizontal',
+    validator(value: string) {
+      return ['horizontal', 'vertical'].includes(value);
+    }
+  }
+});
+console.log(props);
+</script>
